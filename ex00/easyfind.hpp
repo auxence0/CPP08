93170778f691ab8e42cc20abd4e69daa273d3b57
auxence0 <sauvagetauxence@gmail.com>
2026-09-21 15:27:32 +0200

    struct files ex00

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asauvage <asauvage@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/21 11:24:41 by asauvage          #+#    #+#             */
+/*   Updated: 2026/09/21 11:26:36 by asauvage         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+template< typename T >
+void	easyfind( T* integers, int i ) {
+	
+}
+
+#endif
